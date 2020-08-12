7cde26961d8a9e9bea7fd30494f8e1a1046ae53b
Taha Bohra <tahamustafa053@gmail.com>
2020-08-13 02:22:24 +0530

    updated with counter code

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-Console.log("Welcome to my App");
+var counter = document.querySelector(".counter");
+var counter_btn = document.querySelector(".counter-btn");
 
 
-alert("Hello World!");
+var count = 0;
+
+counter_btn.addEventListener("click", function() {
+	count = count + 1;
+	counter.innerHTML = count;
+	if (count == 1000) {
+		counter.innerHTML = count;
+		alert("You have Gained KHUSHI OF AQA MOULA TUS");
+	}
+})

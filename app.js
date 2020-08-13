026f4767855277896a5427c6e62e68dc61028c55
Taha Bohra <tahamustafa053@gmail.com>
2020-08-14 00:48:34 +0530

    reset button

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var counter = document.querySelector(".counter");
 var counter_btn = document.querySelector(".counter-btn");
-
+var reset_btn = document.querySelector(".reset-btn");
 
 var count = 0;
 
@@ -10,5 +10,12 @@ counter_btn.addEventListener("click", function() {
 	if (count == 1000) {
 		counter.innerHTML = count;
 		alert("You have Gained KHUSHI OF AQA MOULA TUS");
+		count = 0;
+		counter.innerHTML = count;
 	}
+});
+
+reset_btn.addEventListener("click", function() {
+	count = 0;
+	counter.innerHTML = count;
 })

182cc02a431ce4290a28e4667e515a4f8185dfb5
Taha Bohra <tahamustafa053@gmail.com>
2020-08-16 16:28:11 +0530

    sample.html updated

--- app.js
+++ app.js
@@ -4,18 +4,26 @@ var reset_btn = document.querySelector(".reset-btn");
 
 var count = 0;
 
-counter_btn.addEventListener("click", function() {
-	count = count + 1;
-	counter.innerHTML = count;
-	if (count == 1000) {
+
+function counter (count) {
+	// body...
+
+	counter_btn.addEventListener("click", function() {
+		count = count + 1;
 		counter.innerHTML = count;
-		alert("You have Gained KHUSHI OF AQA MOULA TUS");
+		if (count == 1000) {
+			counter.innerHTML = count;
+			alert("You have Gained KHUSHI OF AQA MOULA TUS");
+			count = 0;
+			counter.innerHTML = count;
+		}
+	});
+
+	reset_btn.addEventListener("click", function() {
 		count = 0;
 		counter.innerHTML = count;
-	}
-});
+	})
+}
 
-reset_btn.addEventListener("click", function() {
-	count = 0;
-	counter.innerHTML = count;
-})
+
+export counter;
